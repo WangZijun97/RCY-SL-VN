1a1ca84596863b6f411a7f404f66fe4b9618ddfe
Lhwhatever <llinhong.2301+github@gmail.com>
2020-12-01 18:53:33 +0800

    Make Game a functional component
    For implementation of flags and reducers

--- src/Game.js
+++ src/Game.js
@@ -3,25 +3,9 @@ import './App.css';
 import * as data from './data.js';
 import Node from './Node.js';
 
-export class Game extends React.Component {
-    
-    constructor(props) {
-        super(props)
-        this.state = {
-            node: "H1"
-        }
-        this.setNode = this.setNode.bind(this)
-    }
-    
-    setNode(num) {
-        this.setState({node: num})
-    }
-    
-    render() {
-        return (<Node trigger={this.setNode} data={data.getNode(this.state.node)} />)
-    }
+const Game = () => {
+    const [node, setNode] = React.useState("H1");
+    return <Node trigger={setNode} data={data.getNode(node)} />
 }
 
-export default Game
-        
-        
+export default Game;
