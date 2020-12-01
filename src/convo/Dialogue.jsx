f9e66eb808899eddd548b34b2b476cc136a35b07
Lhwhatever <llinhong.2301+github@gmail.com>
2020-12-01 23:10:13 +0800

    Fix multi-responses being concatenated.

--- src/convo/Dialogue.jsx
+++ src/convo/Dialogue.jsx
@@ -27,7 +27,9 @@ const Dialogue = (props) => {
         <div class={combinedSpeakerClass}>{startOfConvo}</div>
         {askedQns.map((qnIndex) => (<React.Fragment key={qnIndex}>
             <div class="self-speech">{convos[qnIndex].q}</div>
-            <div class={combinedSpeakerClass}>{convos[qnIndex].a}</div>
+            {convos[qnIndex].a.map((ans) => (
+                <div class={combinedSpeakerClass}>{ans}</div>
+            ))}
         </React.Fragment>))}
         <div className="button-container">
             {availableQns.map((qnIndex) => <button key={qnIndex} onClick={createButtonHandler(qnIndex)}>
